fba220fdcfba8c240e9c04a6493fec735917613c
Dan Murphy <murphydbuffalo@gmail.com>
2014-06-06 10:54:27 -0400

    Made remaining tests pass by adding functionality to the Recipe and Ingredient classes/models.  Both now can establish connections to the db, query the approprtiate table in the DB and return the appropriate results based on matching recipe.id and ingredient.recipe_id.

--- models/ingredient.rb
+++ models/ingredient.rb
@@ -1,2 +1,34 @@
 class Ingredient
+    attr_reader :name, :recipe_id
+
+	def initialize(args={})
+      @id = args[:id]
+      @name = args[:name]
+      @recipe_id = args[:recipe_id]
+	end
+
+	def self.access_db
+      begin
+        connection = PG.connect(dbname: 'systems_check_recipes')
+        yield(connection)
+      ensure
+      	connection.close
+      end
+	end
+
+	def self.ingredients_query
+      query = "SELECT * FROM ingredients"
+	end
+
+	def self.all
+      ingredients = []
+      query_results = access_db {|conn| conn.exec(ingredients_query)}
+      query_results.each do |row|
+        ingredients << Ingredient.new(:id => row["id"],
+        	:name => row["name"],
+        	:recipe_id => row["recipe_id"]) 
+      end
+      ingredients
+	end
+
 end
--- models/recipe.rb
+++ models/recipe.rb
@@ -1,42 +1,51 @@
 class Recipe
-	attr_reader :id, :name, :directions, :prep_time, :cook_time, :servings
+	attr_reader :id, :name, :instructions, :description
 	
 	def initialize(args={})
-      @id = args[:id]
-      @name = args[:name]
-      @directions = args[:directions] 
-      @prep_time = args[:prep_time]  
-      @cook_time = args[:cook_time] 
-      @servings = args[:servings] 
-      @category_id = args[:category_id]
+    @id = args[:id]
+    @name = args[:name]
+    @instructions = args[:instructions] || "This recipe doesn't have any instructions."
+    @description = args[:description] || "This recipe doesn't have a description."
 	end
 
 	def self.access_db
-      begin
-        connection = PG.connect(dbname: 'systems_check_recipes')
-        yield(connection)
-      ensure
-      	connection.close
-      end
+    begin
+      connection = PG.connect(dbname: 'systems_check_recipes')
+      yield(connection)
+    ensure
+    	connection.close
+    end
 	end
 
 	def self.recipes_query
-      query = "SELECT * FROM recipes"
+    query = "SELECT * FROM recipes"
 	end
 
 	def self.all
-      recipes = []
-      query_results = access_db {|conn| conn.exec(recipes_query)}
-      query_results.each do |row|
-        recipes << Recipe.new(:id => row["id"],
-        	:name => row["name"], 
-        	:directions => row["directions"], 
-        	:servings => row["servings"], 
-        	:prep_time => row["prep_time_in_minutes"],
-        	:cook_time => row["cooking_time_in_minutes"])
-      end
-      recipes
+    recipes = [] 
+    query_results = access_db {|conn| conn.exec(recipes_query)}
+    query_results.each do |row|
+      recipes << Recipe.new(:id => row["id"],
+      	:name => row["name"], 
+      	:instructions => row["instructions"], 
+      	:description => row["description"])
+    end
+    recipes
 	end
 
+	def self.find(id)
+    Recipe.all.each do |recipe|
+      return recipe if id == recipe.id
+    end
+	end
 
+  def ingredients
+    ingredients = []
+    Ingredient.all.each do |ingredient|
+      if ingredient.recipe_id == self.id
+        ingredients << ingredient
+      end
+    end
+      ingredients
+  end
 end
